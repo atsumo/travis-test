f23c9cd0ca4d792f64c54d354b7f52c7520d8b25
atsumo <bachisuta@gmail.com>
2013-12-19 17:32:09 +0900

    add test code

---
+++ sample.js
@@ -0,0 +1,11 @@
+/**
+ * Say Hello
+ * @param name {string}
+ */
+exports.sayHello = function(name) {
+  return name + ' hello!!';
+};
+
+exports.sayGoodBye = function(name) {
+  return name + ' goodbye!';
+}
---
+++ test/test.sample.js
@@ -0,0 +1,16 @@
+/*global describe:true,before:true,after:true,it:true*/
+/*jshint expr:true */
+
+// load configuration
+var sample = require('../');
+
+describe('#sayHello', function() {
+  it('should return atsumo hello! when the value is atsumo', function(done) {
+    sample.sayHello('atsumo').should.equal('atsumo hello!!');
+    done();
+  });
+  it('should return atsumo goodbye when the value is atsumo', function(done) {
+    sample.sayGoodBye('atsumo').should.equal('atsumo goodbye!');
+    done();
+  });
+});
