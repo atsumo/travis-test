925a92350995bb97b6743597aafde59d4da34c70
atsumo <bachisuta@gmail.com>
2014-01-14 16:41:25 +0900

    add coveralls.yml

--- test/test.sample.js
+++ test/test.sample.js
@@ -1,14 +1,15 @@
-/*global describe:true,before:true,after:true,it:true*/
+/*global describe,it*/
 /*jshint expr:true */
 
 // load configuration
 var sample = require('../');
 
-describe('#sayHello', function() {
+describe('#sayHello', function(){
   it('should return atsumo hello! when the value is atsumo', function(done) {
-    sample.sayHello('atsumo').should.equal('atsumo hello!!');
+    sample.sayHello('atsumo').should.equal('atsumo hello??');
     done();
   });
+
   it('should return atsumo goodbye when the value is atsumo', function(done) {
     sample.sayGoodBye('atsumo').should.equal('atsumo goodbye!');
     done();
